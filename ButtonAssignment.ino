99a30ce69ae7df953fb3b716e87c071c67a1de73
cjschw4 <claire.schwartz@emory.edu>
2025-10-29 16:23:34 -0400

    Update ButtonAssignment.ino comments

--- ButtonAssignment.ino
+++ ButtonAssignment.ino
@@ -13,6 +13,7 @@ void setup() {
   // set LED pins to output
   DDRB |= B00111100;
   pinMode(13, OUTPUT);
+  // set button pins to input
   pinMode(A4, INPUT);
   pinMode(A5, INPUT);
 }
@@ -28,6 +29,8 @@ void loop() {
       goRight = true;
     }
   }
+  // move the index of which light is on
+    // wrap around if on an edge
   if (goRight) {
     lightOn += 1;
     if (lightOn >= 4) lightOn = 0;
@@ -35,6 +38,7 @@ void loop() {
     lightOn -= 1;
     if (lightOn < 0) lightOn = 3;
   }
+  // blink current light
   int time = 250; 
   PORTD |= (1 << (lightOn+2));
   delay(time);
